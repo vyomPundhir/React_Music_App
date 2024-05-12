77de3e8ca0cb8d430e13cfd6bf1dfdba20b5e7e0
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-12 23:45:45 +0530

    Sidebar jsx file updated.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,76 +1,59 @@
 import React from 'react'
-import { Home, BarChart, Copy, Bookmark, Users, Settings } from 'lucide-react'
 
-export function Sidebar() {
+const Sidebar = () => {
   return (
-    <aside className="flex h-screen w-16 flex-col items-center overflow-y-auto border-r bg-white py-8">
-      <nav className="flex flex-1 flex-col items-center space-y-6">
-        <a href="#">
-          <svg
-            width="40"
-            height="46"
-            viewBox="0 0 50 56"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M23.2732 0.2528C20.8078 1.18964 2.12023 12.2346 1.08477 13.3686C0 14.552 0 14.7493 0 27.7665C0 39.6496 0.0986153 41.1289 0.83823 42.0164C2.12023 43.5449 23.2239 55.4774 24.6538 55.5267C25.9358 55.576 46.1027 44.3832 48.2229 42.4602C49.3077 41.474 49.3077 41.3261 49.3077 27.8158C49.3077 14.3055 49.3077 14.1576 48.2229 13.1714C46.6451 11.7415 27.1192 0.450027 25.64 0.104874C24.9497 -0.0923538 23.9142 0.00625992 23.2732 0.2528ZM20.2161 21.8989C20.2161 22.4906 18.9835 23.8219 17.0111 25.3997C15.2361 26.7803 13.8061 27.9637 13.8061 28.0623C13.8061 28.1116 15.2361 29.0978 16.9618 30.2319C18.6876 31.3659 20.2655 32.6479 20.4134 33.0917C20.8078 34.0286 19.871 35.2119 18.8355 35.2119C17.8001 35.2119 9.0233 29.3936 8.67815 28.5061C8.333 27.6186 9.36846 26.5338 14.3485 22.885C17.6521 20.4196 18.4904 20.0252 19.2793 20.4196C19.7724 20.7155 20.2161 21.3565 20.2161 21.8989ZM25.6893 27.6679C23.4211 34.9161 23.0267 35.7543 22.1391 34.8668C21.7447 34.4723 22.1391 32.6479 23.6677 27.9637C26.2317 20.321 26.5275 19.6307 27.2671 20.3703C27.6123 20.7155 27.1685 22.7864 25.6893 27.6679ZM36.0932 23.2302C40.6788 26.2379 41.3198 27.0269 40.3337 28.1609C39.1503 29.5909 31.6555 35.2119 30.9159 35.2119C29.9298 35.2119 28.9436 33.8806 29.2394 33.0424C29.3874 32.6479 30.9652 31.218 32.7403 29.8867L35.9946 27.4706L32.5431 25.1532C30.6201 23.9205 29.0915 22.7371 29.0915 22.5892C29.0915 21.7509 30.2256 20.4196 30.9159 20.4196C31.3597 20.4196 33.6771 21.7016 36.0932 23.2302Z"
-              fill="black"
-            />
-          </svg>
-        </a>
-        <a
-          href="#"
-          className="rounded-lg p-1.5 text-gray-700 transition-colors duration-200 hover:bg-gray-100 focus:outline-none"
-        >
-          <Home size={24} />
+    <section className="side-bar flex flex-col justify-start gap-[10px] bg-transparent w-1/4 rounded-lg">
+
+      <div className="side-menu border-[1px] border-[#740505] rounded-lg px-[20px] py-[20px] flex flex-col justify-center gap-[10px] bg-[#0c0606]">
+
+        <a href="/home" className="flex flex-row items-center gap-[20px] hover:bg-pink-950 hover:cursor-pointer transition duration-300 px-[10px] py-[7px] rounded-lg">
+          <img src="src\assets\home.png" alt="" className="w-[23px]"/>
+          <h4 className="font-[600] text-[17px]">Home</h4>
         </a>
 
-        <a
-          href="#"
-          className="rounded-lg p-1.5 text-gray-700 transition-colors duration-200 hover:bg-gray-100 focus:outline-none"
-        >
-          <BarChart size={24} />
+        <a href="/search" className="flex flex-row items-center gap-[20px] hover:bg-pink-950 hover:cursor-pointer transition duration-300 px-[10px] py-[7px] rounded-lg">
+          <img src="src\assets\search.png" alt="" className="w-[23px]" />
+          <h4 className="font-[600] text-[17px]">Search</h4>
         </a>
 
-        <a
-          href="#"
-          className="rounded-lg p-1.5 text-gray-700 transition-colors duration-200 hover:bg-gray-100 focus:outline-none"
-        >
-          <Copy size={24} />
-        </a>
-
-        <a
-          href="#"
-          className="rounded-lg p-1.5 text-gray-700 transition-colors duration-200 hover:bg-gray-100 focus:outline-none"
-        >
-          <Bookmark size={24} />
-        </a>
-
-        <a
-          href="#"
-          className="rounded-lg p-1.5 text-gray-700 transition-colors duration-200 hover:bg-gray-100 focus:outline-none"
-        >
-          <Users size={24} />
-        </a>
-      </nav>
-
-      <div className="flex flex-col items-center space-y-6">
-        <a
-          href="#"
-          className="rounded-lg bg-gray-100 p-1.5 text-gray-700 transition-colors duration-200 focus:outline-none"
-        >
-          <Settings size={24} />
-        </a>
-
-        <a href="#">
-          <img
-            className="h-8 w-8 rounded-full object-cover"
-            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-            alt="User avatar"
-          />
-        </a>
       </div>
-    </aside>
+
+      <div className="library border-[1px] border-[#740505] rounded-lg px-[10px] py-[20px] flex flex-col justify-center gap-[28px] bg-[#0c0606]">
+
+        <div className="flex flex-row justify-between px-[15px]">
+
+          <div className="flex flex-row gap-[10px] hover:cursor-pointer">
+            <img src="src\assets\library.png" alt="" className="w-[23px]" />
+            <h4 className="font-[600] texyt-[17px]">Your Library</h4>
+          </div>
+
+          <div className="flex flex-row gap-[10px]">
+            <img src="src\assets\plus.png" alt="" className="rounded-full p-[3px] hover:bg-pink-950 hover:cursor-pointer transition duration-300 w-[25px] h-[25px]" />
+            <img src="src\assets\right-arrow.png" alt="" className="rounded-full p-[3px] hover:bg-pink-950 hover:cursor-pointer transition duration-300 w-[25px] h-[25px]" />
+          </div>
+
+        </div>
+
+        <div className="flex flex-col justify-start items-start gap-[10px] rounded-lg overflow-y-auto h-[280px] scrollbar-thin scrollbar-track-black scrollbar-thumb-[#1c0707]">
+
+          <div className="w-full bg-[#210707] rounded-lg flex flex-col justify-center items-start gap-[10px] px-[20px] py-[10px]">
+            <h4 className="font-[600] text-[17px]">Create your first playlist</h4>
+            <p className="text-[15px]">It's easy, we'll help you</p>
+            <button className="text-[15px] px-[20px] py-[5px] rounded-3xl bg-pink-900 hover:bg-pink-700 transition duration-300">Create playlist</button>
+          </div>
+
+          <div className="w-full rounded-lg flex flex-col justify-center items-start gap-[10px] px-[20px] py-[10px] bg-[#210707]">
+            <h4 className="font-[600] text-[17px]">Let's find some podcasts to follow</h4>
+            <p className="text-[15px]">We'll help you updated on new episodes</p>
+            <button className="text-[15px] px-[20px] py-[5px] rounded-3xl bg-pink-900 hover:bg-pink-700 transition duration-300">Browse Podcasts</button>
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
   )
 }
+
+export default Sidebar
