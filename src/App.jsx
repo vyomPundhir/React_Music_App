cf80db7f5e8deddd3039ff363be3d43a15ee3b9a
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-12 23:42:27 +0530

    App jsx file updated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Home from './components/Home'
 
 const App = () => {
   return (
-    <div>
-      <Navbar/>
-    </div>
+    <section className="main w-full px-[10px] py-[10px] flex flex-row bg-[#000000] text-white h-screen gap-[10px] scrollbar-thin scrollbar-track-black scrollbar-thumb-[#1c0707]">
+      <Sidebar/>
+      <Home/>
+    </section>
   )
 }
 
