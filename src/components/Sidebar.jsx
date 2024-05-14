9ac5e6679fc18e6a5960fae96ff8f6a80a19261f
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-14 23:17:22 +0530

    Sidebar jsx file updated.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import home from '../assets/home.png'
+import search from '../assets/search.png'
+import plus from '../assets/plus.png'
+import library from '../assets/library.png'
+import arrow from '../assets/right-arrow.png'
+import { Link, NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
@@ -6,15 +12,15 @@ const Sidebar = () => {
 
       <div className="side-menu border-[1px] border-[#740505] rounded-lg px-[20px] py-[20px] flex flex-col justify-center gap-[10px] bg-[#0c0606]">
 
-        <a href="/home" className="flex flex-row items-center gap-[20px] hover:bg-pink-950 hover:cursor-pointer transition duration-300 px-[10px] py-[7px] rounded-lg">
-          <img src="src\assets\home.png" alt="" className="w-[23px]"/>
+        <NavLink to="/home/all" className={({isActive})=>`{isActive ?  "bg-pink-950" : "text-[#979797]"} flex flex-row items-center gap-[20px] hover:bg-pink-950 hover:cursor-pointer transition duration-300 px-[10px] py-[7px] rounded-lg`}>
+          <img src={home} alt="" className="w-[23px]"/>
           <h4 className="font-[600] text-[17px]">Home</h4>
-        </a>
+        </NavLink>
 
-        <a href="/search" className="flex flex-row items-center gap-[20px] hover:bg-pink-950 hover:cursor-pointer transition duration-300 px-[10px] py-[7px] rounded-lg">
-          <img src="src\assets\search.png" alt="" className="w-[23px]" />
+        <NavLink to="/search" className={({isActive})=>`{isActive ? "bg-pink-900" : "text-[#979797]"} flex flex-row items-center  gap-[20px] hover:bg-pink-950 hover:cursor-pointer transition duration-300 px-[10px] py-[7px] rounded-lg`}>
+          <img src={search} alt="" className="w-[23px]" />
           <h4 className="font-[600] text-[17px]">Search</h4>
-        </a>
+        </NavLink>
 
       </div>
 
@@ -23,13 +29,13 @@ const Sidebar = () => {
         <div className="flex flex-row justify-between px-[15px]">
 
           <div className="flex flex-row gap-[10px] hover:cursor-pointer">
-            <img src="src\assets\library.png" alt="" className="w-[23px]" />
+            <img src={library} alt="" className="w-[23px]" />
             <h4 className="font-[600] texyt-[17px]">Your Library</h4>
           </div>
 
           <div className="flex flex-row gap-[10px]">
-            <img src="src\assets\plus.png" alt="" className="rounded-full p-[3px] hover:bg-pink-950 hover:cursor-pointer transition duration-300 w-[25px] h-[25px]" />
-            <img src="src\assets\right-arrow.png" alt="" className="rounded-full p-[3px] hover:bg-pink-950 hover:cursor-pointer transition duration-300 w-[25px] h-[25px]" />
+            <img src={plus} alt="" className="rounded-full p-[3px] hover:bg-pink-950 hover:cursor-pointer transition duration-300 w-[25px] h-[25px]" />
+            <img src={arrow} alt="" className="rounded-full p-[3px] hover:bg-pink-950 hover:cursor-pointer transition duration-300 w-[25px] h-[25px]" />
           </div>
 
         </div>
@@ -45,7 +51,7 @@ const Sidebar = () => {
           <div className="w-full rounded-lg flex flex-col justify-center items-start gap-[10px] px-[20px] py-[10px] bg-[#210707]">
             <h4 className="font-[600] text-[17px]">Let's find some podcasts to follow</h4>
             <p className="text-[15px]">We'll help you updated on new episodes</p>
-            <button className="text-[15px] px-[20px] py-[5px] rounded-3xl bg-pink-900 hover:bg-pink-700 transition duration-300">Browse Podcasts</button>
+            <Link to='/home/podcasts' className="text-[15px] px-[20px] py-[5px] rounded-3xl bg-pink-900 hover:bg-pink-700 transition duration-300">Browse Podcasts</Link>
           </div>
 
         </div>
