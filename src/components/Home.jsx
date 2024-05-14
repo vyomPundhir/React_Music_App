e5b8a6c409d5f3d66e516cbdc3e05d067073c7bb
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-14 23:15:50 +0530

    Home jsx file updated.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,18 @@
-import React from 'react'
-import Navbar from './Navbar'
-// import All from './All'
-// import Music from './Music'
-import Podcasts from './Podcasts'
-import Footer from './Footer'
+import React from 'react';
+import Navbar from './Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
 
 const Home = () => {
   return (
-      <section className="route w-3/4  flex flex-col justify-start gap-[10px] rounded-lg bg-gradient-to-r from-[#140404] to-black h-[517px] ">
-
-        <Navbar/>
-
-        <section className="flex flex-col gap-[20px] rounded-lg overflow-y-auto h-[375px] scrollbar-thin scrollbar-track-black scrollbar-thumb-[#270a0a] my-[20px] px-[20px] py-[10px] ">
-
-          {/* <All/> */}
-          {/* <Music/> */}
-          <Podcasts/>
-          <Footer/>
-
-        </section>
-
+    <section className="route w-3/4 flex flex-col justify-start gap-[10px] rounded-lg bg-gradient-to-r from-[#140404] to-black h-[517px] ">
+      <Navbar />
+      <section className="flex flex-col gap-[20px] rounded-lg overflow-y-auto h-[375px] scrollbar-thin scrollbar-track-black scrollbar-thumb-[#270a0a] my-[20px] px-[20px] py-[10px] ">
+        <Outlet/>
+        <Footer />
       </section>
-  )
-}
+    </section>
+  );
+};
 
-export default Home
+export default Home;
