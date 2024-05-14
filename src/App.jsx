85e4b64cb6f124b437efb0de5f88d9de40d4856d
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-14 23:14:16 +0530

    App jsx file updated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
-import React from 'react'
-import Sidebar from './components/Sidebar'
-import Home from './components/Home'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Search from './components/Search';
+import All from './components/All';
+import Music from './components/Music';
+import Podcasts from './components/Podcasts';
 
 const App = () => {
   return (
-    <section className="main w-full px-[10px] py-[10px] flex flex-row bg-[#000000] text-white h-screen gap-[10px] scrollbar-thin scrollbar-track-black scrollbar-thumb-[#1c0707]">
-      <Sidebar/>
-      <Home/>
-    </section>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="home/" element={<Home />}>
+            <Route path="all" element={<All />} />
+            <Route path="music" element={<Music />} />
+            <Route path="podcasts" element={<Podcasts />} />
+          </Route>
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import Sidebar from './components/Sidebar'
+// import { Outlet } from 'react-router-dom'
+
+// const App = () => {
+//   return (
+//     <section className="main w-full px-[10px] py-[10px] flex flex-row bg-[#000000] text-white h-screen gap-[10px] scrollbar-thin scrollbar-track-black scrollbar-thumb-[#1c0707]">
+//       <Sidebar/>
+//       <Outlet />
+//     </section>
+//   )
+// }
+
+// export default App
