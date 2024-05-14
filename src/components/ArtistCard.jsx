ad527e60788db4c5a89c9a2150b13ad141eeab0b
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-14 23:15:23 +0530

    ArtisrtCard jsx file updated.

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import test from '../assets/download.jfif'
 
 const ArtistCard = () => {
   return (
     <div className="flex flex-col justify-center items-center w-[160px] hover:bg-[#280810] hover:shadow-[0px_5px_40px_-10px_#000000] px-[10px] py-[10px] gap-[5px] rounded-lg transition duration-300 cursor-pointer">
-      <img src="src\assets\download.jfif" className="w-[150px] h-[150px] rounded-full" />
+      <img src={test} className="w-[150px] h-[150px] rounded-full" />
       <div className="flex flex-col justify-center items-center">
         <h4>Artist 1</h4>
         <p>Artist Detail</p>
