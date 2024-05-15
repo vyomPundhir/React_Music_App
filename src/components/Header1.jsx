add3a6f272d971173241de99630bd3cc1d2bdc6b
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-15 23:14:32 +0530

    Header1 jsx file added.

---
+++ src/components/Header1.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import download from '../assets/download.png'
+import bell from '../assets/bell.png'
+
+const Header1 = () => {
+  return (
+      <section className="mx-[10px] px-[10px] py-[10px] rounded-lg flex flex-row justify-end items-center gap-[20px]">
+
+        <Link to="/signup" className="px-[15px] py-[5px] bg-pink-950 rounded-3xl cursor-pointer hover:bg-pink-700 hover:border-none transition duration-300">Sign up</Link>
+
+        <Link to="/login" className=" bg-pink-950 px-[25px] py-[5px] rounded-3xl cursor-pointer hover:bg-pink-700 hover:border-none transition duration-300">Login</Link>
+
+        <Link to="/layout2/download" className="flex flex-row justify-center items-center gap-[10px] bg-[black] border-[1px] px-[15px] py-[5px] rounded-3xl cursor-pointer hover:bg-pink-700 hover:border-none transition duration-300">
+
+          <img className="w-[20px] border-[1px] rounded-xl" src={download} alt="" />
+
+          <p>Install App</p>
+
+        </Link>
+
+        <Link to='/layout2/new' className="flex flex-row justify-center items-center cursor-pointer bg-[#0c0606] border-[1px] px-[7px] py-[7px] rounded-full  hover:bg-pink-700 hover:border-none transition duration-300">
+
+          <img className="w-[20px] h-[20px]" src={bell} alt=""/>
+
+        </Link>
+
+      </section>
+  )
+}
+
+export default Header1
