4f1eab57bd4b730ac84d8b1323c4664d5e1effb1
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-15 23:15:20 +0530

    Layout 2 jsx file updated.

---
+++ src/components/Layout2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header1 from './Header1'
+
+const Layout2 = () => {
+  return (
+    <section className="route w-3/4 flex flex-col justify-start gap-[10px] rounded-lg bg-gradient-to-r from-[#140404] to-black h-[517px] ">
+      <Header1/>
+      <Outlet/>
+    </section>
+  )
+}
+
+export default Layout2
