1c3f0f867ebacfe2822baa6b816156dfc1295ae9
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-15 23:20:04 +0530

    Trending Episodes jsx file added.

---
+++ src/components/TrendingEpisodes.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import AlbumCard from './AlbumCard'
+
+
+const TrendingEpisodes = () => {
+  return (
+    <section className="flex flex-col gap-[20px]">
+
+      <div className="flex flex-col gap-[20px]">
+
+
+        <h4 className="hover:cursor-pointer hover:underline text-[27px] font-[700]">Trending Episodes</h4>
+
+
+        <div className="flex flex-row items-center justify-start flex-wrap gap-[10px]">
+
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+        </div>
+
+      </div>
+
+    </section>
+  )
+}
+
+export default TrendingEpisodes
