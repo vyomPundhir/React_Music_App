3f016fa9dc7b67f2cc0bde2f8b3b02ff1c6e1eda
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-15 23:14:11 +0530

    Featured Charts jsx file added.

---
+++ src/components/FeaturedCharts.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import AlbumCard from './AlbumCard'
+
+function FeaturedCharts() {
+  return (
+    <section className="flex flex-col gap-[20px]">
+
+      <div className="flex flex-col gap-[20px]">
+
+
+        <h4 className="hover:cursor-pointer hover:underline text-[27px] font-[700]">Featured Charts</h4>
+
+
+        <div className="flex flex-row items-center justify-start flex-wrap gap-[10px]">
+
+          <AlbumCard/>
+          <AlbumCard />
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+          <AlbumCard/>
+        </div>
+
+      </div>
+
+    </section>
+  )
+}
+
+export default FeaturedCharts
