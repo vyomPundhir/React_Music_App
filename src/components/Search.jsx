d9df5eb64c36839366634852d483578e83216d12
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-17 23:22:20 +0530

    Search jsx file added.

---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <section className="route w-3/4  flex flex-col justify-start gap-[10px] rounded-lg bg-gradient-to-r from-[#140404] to-black h-[517px] ">
+      Search Works
+    </section>
+  )
+}
+
+export default Search
