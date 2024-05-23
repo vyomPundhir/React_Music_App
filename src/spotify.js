b2df6f734836466cc43cd6235af48b2a1cf35b59
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-23 23:57:58 +0530

    spotify js file updated.

--- src/spotify.js
+++ src/spotify.js
@@ -6,7 +6,7 @@ const clientId = "d0cbd6f6defc4a418d298a3816c71ae9";
 const redirectUri = "http://localhost:5173/";
 const scopes = ["user-library-read", "playlist-read-private"];
 
-export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_doialog=true`;
+export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
 
 const apiClient = axios.create({
   baseURL: "https://api.spotify.com/v1/"
