0502ad323c302aef189f34bfeae32c61f20c9ef1
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-26 23:08:48 +0530

    App jsx file updated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+import Login from "./components/Login" 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
@@ -6,8 +7,6 @@ import Search from './components/Search';
 import All from './components/All';
 import Music from './components/Music';
 import Podcasts from './components/Podcasts';
-import Signup from './components/Signup';
-import Login from './components/Login';
 import Download from './components/Download';
 import Layout2 from './components/Layout2';
 import New from './components/New';
@@ -18,61 +17,60 @@ import MusicPlayerPlaylists from './components/MusicPlayerPlaylists';
 import OriginalPodcasts from './components/OriginalPodcasts';
 import PopularAlbums from './components/PopularAlbums';
 import TrendingEpisodes from './components/TrendingEpisodes';
-import { setClientToken } from './spotify';
+
 
 const App = () => {
+
   const [token, setToken] = useState("");
 
   useEffect(() => {
-    const token = window.localStorage.getItem("token");
-    const hash = window.location.hash;
-    window.location.hash = "";
-    if (!token && hash) {
-      const _token = hash.split("&")[0].split("=")[1];
-      window.localStorage.setItem("token", _token);
-      setToken(_token);
-      setClientToken(_token)
-    } else {
-      setToken(token);
-      setClientToken(token);
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if(!token && hash){
+      token=hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
     }
-  }, []);
+    setToken(token)
+  }, [])
   
+  return (
+    <div>
+      { !token ?
+      <Login/> :
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="home/" element={<Home />}>
+                <Route path="all" element={<All />} />
+                <Route path="music" element={<Music />} />
+                <Route path="podcasts" element={<Podcasts />} />
+                <Route path="popularArtists" element={<PopularArtists />} />
+                <Route path="popularAlbums" element={<PopularAlbums />} />
+                <Route path="popularRadio" element={<PopularRadio />} />
+                <Route path="featuredCharts" element={<FeaturedCharts />} />
+                <Route path="musicPlayerPlaylists" element={<MusicPlayerPlaylists />} />
+                <Route path="originalPodcasts" element={<OriginalPodcasts />} />
+                <Route path="trendingEpisodes" element={<TrendingEpisodes />} />
+    
+              </Route>
+    
+              <Route path="search" element={<Search />} />
+    
+              <Route path='layout2/' element={<Layout2 />}>
+                <Route path='download' element={<Download/>}/>
+                <Route path='new' element={<New/>}/>
+              </Route>
+    
+            </Route>
 
-  return !token ? (
-  <Login/>
-) : (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="home/" element={<Home />}>
-            <Route path="all" element={<All />} />
-            <Route path="music" element={<Music />} />
-            <Route path="podcasts" element={<Podcasts />} />
-            <Route path="popularArtists" element={<PopularArtists />} />
-            <Route path="popularAlbums" element={<PopularAlbums />} />
-            <Route path="popularRadio" element={<PopularRadio />} />
-            <Route path="featuredCharts" element={<FeaturedCharts />} />
-            <Route path="musicPlayerPlaylists" element={<MusicPlayerPlaylists />} />
-            <Route path="originalPodcasts" element={<OriginalPodcasts />} />
-            <Route path="trendingEpisodes" element={<TrendingEpisodes />} />
-
-          </Route>
-
-          <Route path="search" element={<Search />} />
-
-          <Route path='layout2/' element={<Layout2 />}>
-            <Route path='download' element={<Download/>}/>
-            <Route path='new' element={<New/>}/>
-          </Route>
-
-        </Route>
-        
-        <Route path='/signup' element={<Signup/>} />
-        <Route path='/login' element={<Login/>} />
-      </Routes>
-    </Router>
-  );
+            <Route path='/login' element={<Login/>} />
+          </Routes>
+        </Router>
+    }
+    </div>
+  )
 };
 
 export default App;
