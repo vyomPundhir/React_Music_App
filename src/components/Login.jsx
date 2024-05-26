0a51c99db605b533350d77b87e3c80382bcc0fdd
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-26 23:09:20 +0530

    Login jsx file updated.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import headphones from '../assets/headphones.png'
 import Footer from './Footer'
-// import { Link } from 'react-router-dom'
-import { loginEndpoint } from '../spotify'
+// import { authEndpoint, clientId, redirectUri, scopes } from '../spotify'
 
 const Login = () => {
+  const authEndpoint = "https://accounts.spotify.com/authorize?";
+  const clientId = "d0cbd6f6defc4a418d298a3816c71ae9";
+  const redirectUri = "http://localhost:5173/";
+  const scopes = [
+    // "user-library-read",
+    // "playlist-read-private",
+    'user-read-email',
+    'user-read-private',
+    'user-modify-playback-state',
+    'user-read-playback-state',
+    'user-read-currently-playing',
+    'user-read-recently-played',
+    'user-read-playback-position',
+    'user-top-read'
+  ];
+
+
   return (
     <section className='bg-[black] text-white'>
       <header className='px-[20px] py-[20px] border-b-[1px] border-b-[#585858]'>
@@ -14,27 +30,16 @@ const Login = () => {
         </div>
       </header>
 
-      <section className='flex flex-col justify-start items-center gap-[30px] bg-gradient-to-r from-[#140404] to-black h-[450px] py-[10px]'>
+      <section className='flex flex-col justify-center items-center gap-[50px] bg-gradient-to-r from-[#140404] to-black h-[450px] py-[10px]'>
 
           <h3 className='text-[35px] font-[700]'>Login to Music Player</h3>
 
           <form action="" className="w-[330px] flex flex-col gap-[20px] border-[1px] border-[#575757] rounded-lg px-[30px] pb-[30px] pt-[20px]">
-
-            {/* <div className="flex flex-col justify-start items-start gap-[5px]">
-              <label for="email">Email</label>
-              <input type="email" id='email' required className="w-full bg-transparent border-[1px] rounded-md"/>
-            </div> */}
-
-            {/* <div className="flex flex-col justify-start items-start gap-[5px]">
-              <label htmlFor="password">Password</label>
-              <input type="password" id='password' required className="w-full bg-transparent border-[1px] rounded-md"/>
-            </div> */}
-
-            <a href={loginEndpoint} className="w-full bg-pink-900 hover:bg-pink-700 transition duration-300 rounded-lg py-[5px]">Login</a>
+            
+            <div className='text-center text-[#7e7d7d]'>Click on the Button to Login to Music Player</div>
+            <a href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`} className="w-full bg-pink-900 hover:bg-pink-700 transition duration-300 rounded-lg py-[5px] text-center">Login</a>
 
           </form>
-
-          {/* <p className='text-white'>Don't have an account? <span><Link to="/signup" className="text-pink-500 hover:underline">Sign up</Link></span></p> */}
 
       </section>
 
