44ed1de2aac26c2180491e560252caaa3e15cef1
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-06-04 23:27:21 +0530

    Popular Artists jsx file updated.

--- src/components/PopularArtists.jsx
+++ src/components/PopularArtists.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 import ArtistCard from './ArtistCard'
+import axios from 'axios'
+// import axios from 'axios'
 
 const PopularArtists = () => {
+
+  // const [artists, setArtists] = useState([])
+  // setToken(window.localStorage.getItem("token"))
+
+  const showArtists = async (e) => {
+    e.preventDefault()
+    const {data} = await axios.get("https://api.spotify.com/v1/artists", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        q: {
+          ids: "2CIMQHirSU0MQqyYHq0eOx,57dN52uHvrHOxijzpIgu3E,1vCWHaC5f2uS3yhpwWbIA6"
+        }
+      }
+    })
+
+    console.log(data)
+  }
+
+
+
   return (
     <section className="flex flex-col gap-[20px]">
 
