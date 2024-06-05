4f2539545abe410ccf648901c09931588cd4a599
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-06-05 23:51:10 +0530

    Album card jsx file updated.

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import test from '../assets/download.jfif'
 
-const AlbumCard = () => {
+const AlbumCard = ({albumImage, albumName, albumDetail}) => {
   return (
     <div className="flex flex-col justify-center items-center w-[160px] hover:bg-[#280810] hover:shadow-[0px_5px_40px_-10px_#000000] px-[10px] py-[10px] gap-[5px] rounded-lg transition duration-300 cursor-pointer">
-      <img src={test} className="w-[150px] h-[150px] rounded-lg" />
+      <img src={albumImage} className="w-[150px] h-[150px] rounded-lg" />
       <div className="flex flex-col justify-center items-center">
-        <h4>Album 1</h4>
-        <p>Album Detail</p>
+        <h4 className='text-center'>{albumName}</h4>
+        <p className='text-[#7c7c7c]'>{albumDetail}</p>
       </div>
     </div>
 
