fc9316092177a9f1f7d173c961a8828753d5c65b
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-06-05 23:52:09 +0530

    Search jsx file updated.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,21 @@
 import axios from 'axios'
 import React, { useState } from 'react'
-// import Footer from './Footer';
 import { Link } from 'react-router-dom';
 import user from "../assets/user.png"
+import ArtistCard from './ArtistCard';
+import AlbumCard from './AlbumCard';
 
 const Search = () => {
-  const [artists, setArtists] = useState([])
+  const [artists, setArtists] = useState([]);
+  const [albums, setAlbums] = useState([])
+  const [episodes, setEpisodes] = useState([])
+  const [tracks, setTracks] = useState([])
+  const [shows, setShows] = useState([])
+  const [playlists, setPlaylists] = useState([])
   const [searchKey, setSearchKey] = useState("");
-  const token = window.localStorage.getItem("token")
+  let token = window.localStorage.getItem("token")
 
-  const searchArtists = async (e) => {
+  const search = async (e) => {
     e.preventDefault()
     const {data} = await axios.get("https://api.spotify.com/v1/search", {
       headers: {
@@ -17,46 +23,118 @@ const Search = () => {
       },
       params: {
         q : searchKey,
-        type: "artist"
+        type: "artist,track,album,playlist,show,episode",
+        limit: 10,
       }
     })
+    console.log(data)
     setArtists(data.artists.items)
-  }
+    setAlbums(data.albums.items)
+    setTracks(data.tracks.items)
+    setPlaylists(data.playlists.items)
+    setShows(data.shows.items)
+    setEpisodes(data.episodes.items)
 
-  const renderArtists = () => {
-    return artists.map(artist => (
-      <div key={artist.id} className="flex flex-col justify-center items-center w-[160px] hover:bg-[#280810] hover:shadow-[0px_5px_40px_-10px_#000000] px-[10px] py-[10px] gap-[5px] rounded-lg transition duration-300 cursor-pointer">
-        {artist.images.length ? <img className='w-[150px] h-[150px] rounded-lg' src={artist.images[0].url}/> : <img className='w-[150px] h-[150px] rounded-lg' src={user}/>}
-        {artist.name}
-      </div>
-    ))
   }
 
   const  logout = () => {
-    token = window.localStorage.removeItem("token")
+    window.localStorage.removeItem("token")
   }
 
   return (
     <section className="route w-3/4  flex flex-col justify-start gap-[30px] rounded-lg bg-gradient-to-r from-[#140404] to-black h-[517px] px-[10px] py-[10px]">
-      <form onSubmit={searchArtists} className="flex flex-row justify-between items-center">
+      <form onSubmit={search} className="flex flex-row justify-between items-center ">
         <input className='rounded-3xl px-[15px] py-[7px] hover:border-[1px] hover:border-[#a8a8a8] bg-[#31313162] transition duration-300 cursor-pointer w-[500px]' type="text" onChange={e => setSearchKey(e.target.value)} placeholder="Search what you want to play."/>
         <div className='flex flex-row justify-center items-center gap-[20px]'>
-          <button type={"submit"} className="flex flex-row justify-center items-center gap-[10px] bg-[black] border-[1px] px-[15px] py-[5px] rounded-3xl cursor-pointer hover:bg-pink-700 hover:border-none transition duration-300">Search</button>
+          <button type="submit" onClick={search} className="flex flex-row justify-center items-center gap-[10px] bg-[black] border-[1px] px-[15px] py-[5px] rounded-3xl cursor-pointer hover:bg-pink-700 hover:border-none transition duration-300">Search</button>
           <Link to="/login" onClick={logout} className="flex flex-row justify-center items-center gap-[10px] bg-[black] border-[1px] px-[15px] py-[5px] rounded-3xl cursor-pointer hover:bg-pink-700 hover:border-none transition duration-300">Logout</Link>
         </div>
       </form>
 
       {
         searchKey ? <div className='w-full flex flex-row flex-wrap justify-start items-center gap-[30px] overflow-y-auto scrollbar-thin scrollbar-track-black scrollbar-thumb-[#1c0707] px-[20px] py-[20px] rounded-xl'>
-        {
-          renderArtists()
-        }
-      </div> : <h2 className="text-[30px] text-center flex flex-row justify-center items-center w-full h-full">Search the item you want to search</h2>
 
+        {
+          tracks.map(track => (
+      
+            <div key={track.id}>
+              {
+                track.album.images.length ? <AlbumCard albumImage={track.album.images[0].url} albumName={track.name} albumDetail={track.type} /> : 
+                <AlbumCard albumImage={user} albumName={track.name} albumDetail={track.type} />
+              }
+      
+            </div>
+          ))
+        }
+
+        {
+          playlists.map(playlist => (
+      
+            <div key={playlist.id}>
+              {
+                playlist.images.length ? <AlbumCard albumImage={playlist.images[0].url} albumName={playlist.name} albumDetail={playlist.type} /> : 
+                <AlbumCard albumImage={user} albumName={playlist.name} albumDetail={playlist.type} />
+              }
+      
+            </div>
+          ))
+        }
+
+        {
+          albums.map(album => (
+      
+            <div key={album.id}>
+              {
+                album.images.length ? <AlbumCard albumImage={album.images[0].url} albumName={album.name} albumDetail={album.type} /> : 
+                <AlbumCard albumImage={user} albumName={album.name} albumDetail={album.type} />
+              }
+      
+            </div>
+          ))
+        }
+
+        {
+          artists.map(artist => (
+      
+            <div key={artist.id}>
+              {
+                artist.images.length ? <ArtistCard artistImage={artist.images[0].url} artistName={artist.name} artistDetail={artist.type} /> : 
+                <ArtistCard artistImage={user} artistName={artist.name} artistDetail={artist.type} />
+              }
+      
+            </div>
+          ))
+        }
+
+        {
+          episodes.map(episode => (
+      
+            <div key={episode.id}>
+              {
+                episode.images.length ? <AlbumCard albumImage={episode.images[0].url} albumName={episode.name} albumDetail={episode.type} /> : 
+                <AlbumCard albumImage={user} albumName={episode.name} albumDetail={episode.type} />
+              }
+      
+            </div>
+          ))
+        }
+
+        {
+          shows.map(show => (
+      
+            <div key={show.id}>
+              {
+                show.images.length ? <AlbumCard albumImage={show.images[0].url} albumName={show.name} albumDetail={show.type} /> : 
+                <AlbumCard albumImage={user} albumName={show.name} albumDetail={show.type} />
+              }
+      
+            </div>
+          ))
+        }
+
+        </div> : <h2 className="text-[30px] text-center flex flex-row justify-center items-center w-full h-full">Search the item you want to search</h2>
       }
 
-      
-      
     </section>
   )
 }
