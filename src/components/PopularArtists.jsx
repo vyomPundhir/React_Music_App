8926f271d6bac6449b7b13f6af5fa0d2086a5b90
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-06-06 21:19:19 +0530

    Popular artists jsx file updated.

--- src/components/PopularArtists.jsx
+++ src/components/PopularArtists.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ArtistCard from './ArtistCard'
+import user from "../assets/user.png"
 import axios from 'axios'
-// import axios from 'axios'
 
 const PopularArtists = () => {
 
-  // const [artists, setArtists] = useState([])
-  // setToken(window.localStorage.getItem("token"))
+  const [artists, setArtists] = useState([])
+  let token = window.localStorage.getItem("token")
 
-  const showArtists = async (e) => {
-    e.preventDefault()
-    const {data} = await axios.get("https://api.spotify.com/v1/artists", {
-      headers: {
-        Authorization: `Bearer ${token}`
-      },
-      params: {
-        q: {
-          ids: "2CIMQHirSU0MQqyYHq0eOx,57dN52uHvrHOxijzpIgu3E,1vCWHaC5f2uS3yhpwWbIA6"
-        }
+  useEffect(() => {
+    const fetchArtists = async () => {
+      try {
+        const {data} = await axios.get("https://api.spotify.com/v1/artists", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+          params: {
+            ids : "1wRPtKGflJrBx9BmLsSwlU,4YRxDV8wJFPHPTeXepOstw,1mYsTxnqsietFxj1OgoGbG,4zCH9qm4R2DADamUHMCa6O,5wJ1H6ud777odtZl5gG507,0y59o4v8uw5crbN9M3JiL1,6Mv8GjQa7LKUGCAqa9qqdb,1mBydYMVBECdDmMfE2sEUO,2oSONSC9zQ4UonDKnLqksx,2FKWNmZWDBZR4dE5KX4plR,70B80Lwx2sxti0M1Ng9e8K,6WOdPJmexxFINcKMkP2jMG,4fEkbug6kZzzJ8eYX6Kbbp,4f7KfxeHq9BiylGmyXepGt,1dVygo6tRFXC8CSWURQJq2,74OaRjmyh0XyRZsQQQ5l7c,1SJOL9HJ08YOn92lFcYf8a,5r3wPya2PpeTTsXsGhQU8O,09UmIX92EUH9hAK4bxvHx6,6LEG9Ld1aLImEFEVHdWNSB"
+          }
+        });
+        console.log(data)
+        setArtists(data.artists);
+      } catch (error) {
+        console.error('Error fetching artists', error);
       }
-    })
-
-    console.log(data)
-  }
-
-
+    };
+  
+    fetchArtists()
+  }, [token])
 
   return (
     <section className="flex flex-col gap-[20px]">
@@ -37,16 +40,19 @@ const PopularArtists = () => {
 
         <div className="flex flex-row items-center justify-start flex-wrap gap-[10px]">
 
-          <ArtistCard/>
-          <ArtistCard/>
-          <ArtistCard/>
-          <ArtistCard/>
-          <ArtistCard/>
-          <ArtistCard/>
-          <ArtistCard/>
-          <ArtistCard/>
-          <ArtistCard/>
-          <ArtistCard/>
+        {
+          artists.map(artist => (
+      
+            <div key={artist.id}>
+              {
+                artist.images.length ? <ArtistCard artistImage={artist.images[0].url} artistName={artist.name} artistDetail={artist.type} /> : 
+                <ArtistCard artistImage={user} artistName={artist.name} artistDetail={artist.type} />
+              }
+      
+            </div>
+          ))
+        }
+
         </div>
 
       </div>
