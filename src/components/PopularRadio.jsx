289161991256e0660762abc0da51e277d239b177
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-06-07 23:53:05 +0530

    Popular Radio jsx file updated.

--- src/components/PopularRadio.jsx
+++ src/components/PopularRadio.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AlbumCard from './AlbumCard'
+import user from "../assets/user.png"
+import axios from 'axios'
 
 function PopularRadio() {
+
+  const [playlists, setPlaylists] = useState([])
+  let token = window.localStorage.getItem("token")
+
+  useEffect(() => {
+    const fetchPlaylists = async () => {
+      try {
+        const {data} = await axios.get("https://api.spotify.com/v1/browse/featured-playlists", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+          params: {
+            limit: 20,
+          }
+        });
+        console.log(data)
+
+      } catch (error) {
+        console.error('Error fetching radios', error);
+      }
+    };
+  
+    fetchPlaylists()
+  }, [token])
+
   return (
     <section className="flex flex-col gap-[20px]">
 
@@ -13,21 +40,19 @@ function PopularRadio() {
 
         <div className="flex flex-row items-center justify-start flex-wrap gap-[10px]">
 
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
+          {/* {
+            playlists.map(album => (
+        
+              <div key={album.id}>
+                {
+                  album.images.length ? <AlbumCard albumImage={album.images[0].url} albumName={album.name} albumDetail={album.type} /> : 
+                  <AlbumCard artistImage={user} albumName={album.name} albumDetail={album.type} />
+                }
+        
+              </div>
+            ))
+          } */}
+
         </div>
 
       </div>
