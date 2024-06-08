87adf0a0a66b7f7192c03452d197d2938c0400ed
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-06-09 00:02:59 +0530

    FeaturedCharts jsx file updated.

--- src/components/FeaturedCharts.jsx
+++ src/components/FeaturedCharts.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AlbumCard from './AlbumCard'
+import user from "../assets/user.png"
+import axios from 'axios'
 
 function FeaturedCharts() {
+
+  const [playlists, setPlaylists] = useState([])
+  let token = window.localStorage.getItem("token")
+
+  useEffect(() => {
+    const fetchPlaylists = async () => {
+      try {
+        const {data} = await axios.get("https://api.spotify.com/v1/browse/featured-playlists", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+          params: {
+            limit: 20,
+          }
+        });
+        console.log(data)
+        setPlaylists(data.playlists.items)
+
+      } catch (error) {
+        console.error('Error fetching featured charts', error);
+      }
+    };
+  
+    fetchPlaylists()
+  }, [token])
+
   return (
     <section className="flex flex-col gap-[20px]">
 
@@ -13,21 +41,19 @@ function FeaturedCharts() {
 
         <div className="flex flex-row items-center justify-start flex-wrap gap-[10px]">
 
-          <AlbumCard/>
-          <AlbumCard />
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
+          {
+            playlists.map(playlist => (
+        
+              <div key={playlist.id}>
+                {
+                  playlist.images.length ? <AlbumCard albumImage={playlist.images[0].url} albumName={playlist.name} albumDetail={playlist.type} /> : 
+                  <AlbumCard albumImage={user} albumName={playlist.name} albumDetail={playlist.type} />
+                }
+        
+              </div>
+            ))
+          }
+
         </div>
 
       </div>
