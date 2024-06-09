ff626bafcfd2a5fc3aea0dbcda795c3565ac0d50
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-06-10 00:23:08 +0530

    Music player Playlists jsx file updated.

--- src/components/MusicPlayerPlaylists.jsx
+++ src/components/MusicPlayerPlaylists.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AlbumCard from './AlbumCard'
+import user from "../assets/user.png"
+import axios from 'axios'
 
 const MusicPlayerPlaylists = () => {
+
+  const [music, setMusic] = useState([])
+  let token = window.localStorage.getItem("token")
+
+  useEffect(() => {
+    const fetchMusic = async () => {
+      try {
+        const {data} = await axios.get("https://api.spotify.com/v1/me/top/tracks", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+          params: {
+            limit: 50
+          }
+        });
+        console.log(data)
+        setMusic(data.items)
+        
+
+      } catch (error) {
+        console.error('Error fetching Music', error);
+      }
+    };
+  
+    fetchMusic()
+  }, [token])
+
   return (
     <section className="flex flex-col gap-[20px]">
 
@@ -13,21 +42,19 @@ const MusicPlayerPlaylists = () => {
 
         <div className="flex flex-row items-center justify-start flex-wrap gap-[10px]">
 
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
+          {
+            music.map(item => (
+        
+              <div key={item.id}>
+                {
+                  item.album.images.length ? <AlbumCard albumImage={item.album.images[0].url} albumName={item.name} albumDetail={item.album.artists[0].name} /> : 
+                  <AlbumCard albumImage={user} albumName={item.name} albumDetail={item.album.artists.name} />
+                }
+        
+              </div>
+            ))
+          }
+
         </div>
 
       </div>
