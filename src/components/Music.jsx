3678a785886686dee61932777f6ce767331967d6
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-06-10 00:22:01 +0530

    Music jsx file updated.

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AlbumCard from './AlbumCard'
-
+import user from "../assets/user.png"
+import axios from 'axios'
 
 const Music = () => {
+
+  // 0JQ5DAqbMKFz6FAsUtgAab,0JQ5DAt0tbjZptfcdMSKl3,0JQ5DAqbMKFHCxg5H5PtqW,0JQ5DAqbMKFKSopHMaeIeI
+  const [items, setItems] = useState([])
+  let token = window.localStorage.getItem("token")
+
+  useEffect(() => {
+    const fetchPlaylists = async () => {
+      try {
+        const {data} = await axios.get("https://api.spotify.com/v1/browse/categories/0JQ5DAt0tbjZptfcdMSKl3/playlists", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+          params: {
+            limit: 30,
+          }
+        });
+        console.log(data)
+        setItems(data.playlists.items)
+
+      } catch (error) {
+        console.error('Error fetching recommendations', error);
+      }
+    };
+  
+    fetchPlaylists()
+  }, [token])
+
   return (
     <section className="flex flex-col gap-[20px]">
 
@@ -14,21 +42,17 @@ const Music = () => {
 
         <div className="flex flex-row items-center justify-start flex-wrap gap-[10px]">
 
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
-          <AlbumCard/>
+        {
+            items.map(item => (
+              <div key={item.id}>
+                {
+                  item.images.length ? <AlbumCard albumImage={item.images[0].url} albumName={item.name} albumDetail={item.type} /> : 
+                  <AlbumCard albumImage={user} albumName={item.images[0].url} albumDetail={item.type} />
+                }
+              </div>
+            ))
+          }
+
         </div>
 
       </div>
