bb0226a07da669363c4f14a547d4490b5d1e5841
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-06-27 23:26:21 +0530

    Album jsx file updated.

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Header1 from './Header1';
+import TrackCard from './TrackCard';
 
 const Album = () => {
 
@@ -32,42 +33,81 @@ const Album = () => {
   if (!album) return <div>Loading...</div>;
 
   return (
-    <section className="route w-3/4 flex flex-col justify-start gap-[10px] rounded-lg bg-gradient-to-r from-[#140404] to-black h-[517px]">
+    <section className="route w-3/4 flex flex-col justify-start gap-[10px] rounded-lg bg-gradient-to-r from-[#140404] to-black h-[517px] ">
 
       <Header1 />
 
-      <section className="">
-        <div className="flex flex-row items-center justify-start">
+      <section className="px-[15px] flex flex-col gap-[20px] overflow-y-auto h-[445px] scrollbar-thin scrollbar-track-black scrollbar-thumb-[#270a0a]">
+        
+        <div className="flex flex-row items-start justify-start gap-[15px]">
+
           <img src={album.images[0].url} alt="image" className="w-[150px] h-[150px] rounded-md" />
-          <div className='flex flex-col items-start justify-center '>
-            <p>{album.type.toUpperCase()}</p>
-            <h1>{album.name}</h1>
+
+          <div className='flex flex-col items-start justify-start '>
+            <p className='text-[14px] font-[500]'>{album.type.toUpperCase()}</p>
+            <h1 className='text-[50px] font-[700]'>{album.name}</h1>
             <p>
               {
                 album.artists.map(artist => (
-                  <span key={artist.id}>
+                  <span key={artist.id} className='text-[13px] font-[500]'>
                     {artist.name} | </span>
                 ))
               }
+              <span  className='text-[13px] font-[500]'>{album.release_date.split("-")[0]} | {album.total_tracks} songs 
+                {/* {
+                album.tracks.items.map(item => (
+                  <span key={item.id}>
+                    {item.duration_ms}
+                  </span>
+                ))
+                } */}
+              </span>
             </p>
           </div>
         </div>
-        <div className=""></div>
+
+        <hr className='h-[1px] border-[#5f5f5f]' />
+
+        <div className="flex flex-col gap-[10px] px-[10px]">
+          <div className='flex flex-row justify-between px-[10px]'>
+            <span className='flex flex-row gap-[20px]'>
+              <span className='text-[14px] text-[#adadad]'>#</span>
+              <span className='text-[14px] text-[#adadad]'>Title</span>
+            </span>
+            <span className='text-[14px] text-[#adadad]'>Duration</span>
+          </div>
+          
+          <div className=''>
+          {tracks.length > 0 ? (
+              tracks.map((track) => (
+                <Link to={`/track/${track.id}`} key={track.id} className='flex flex-col gap-[20px] pb-[10px]'>
+                  <TrackCard
+                    trackNumber={track.track_number}
+                    trackName={track.name}
+                    trackArtists={track.artists.map((artist) => artist.name).join(', ')}
+                    trackTime={millisToMinutesAndSeconds(track.duration_ms)}
+                  />
+                </Link>
+              ))
+            ) : (
+              <p>No tracks available</p>
+            )}
+
+          </div>
+
+        </div>
+
       </section>
 
     </section>
-
-    // <div>
-    //   <h1>{album.name}</h1>
-    //   <img src={album.images[0]?.url} alt={album.name} className='w-[100px] h-[100px]' />
-    //   <ul>
-    //     {tracks.map(track => (
-    //       <li key={track.id}>{track.name}</li>
-    //     ))}
-    //   </ul>
-    // </div>
   );
 
 }
+
+const millisToMinutesAndSeconds = (millis) => {
+  const minutes = Math.floor(millis / 60000);
+  const seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+};
 
 export default Album
