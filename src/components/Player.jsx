705752efee41fa9d0fd827dc88dc44aee74de3eb
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-07-03 23:30:28 +0530

    Player jsx file updated.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Player = () => {
   return (
-    <div>Player</div>
+    <section>
+      <div className="currentTrack">
+
+        <div>
+          <img src={trackImage} alt="" />
+        </div>
+
+        <div>
+          <h3>{trackName}</h3>
+          <p>{trackArtist}</p>
+        </div>
+
+        <div>
+          <img src={addToPlaylist} alt="" />
+        </div>
+
+      </div>
+
+      <div className="playerItems"></div>
+      
+    </section>
   )
 }
 
