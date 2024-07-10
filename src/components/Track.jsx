2f0aef73bc0877d9f6a6c43d954c94832c03a42c
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-07-10 23:59:14 +0530

    Track jsx file updated.

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Header1 from './Header1';
 import TrackCard from './TrackCard';
 
